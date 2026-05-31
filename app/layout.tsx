88363e289ffd23f36fc0c5cc753dc444ddaa0b51
ridwanariana-back <ridwanariana77@gmail.com>
2026-05-31 13:13:22 +0700

    add loading bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 // 1. Impor SessionProvider
 import { SessionProvider } from "next-auth/react";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NextTopLoader 
+          color="#2563eb" // 🔥 Warna biru mantap (Tailwind blue-600) menyesuaikan tema SIMS abang
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3} // Ketebalan loading bar dalam pixel
+          crawl={true}
+          showSpinner={false} // Matikan spinner bundar di pojok kanan agar clean (hanya bar atas saja)
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2563eb,0 0 5px #2563eb"
+        />
         {/* 2. Bungkus children dengan SessionProvider */}
         <SessionProvider>
           {children}
